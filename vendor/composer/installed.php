29b36eba196d5022edb8bf806c1c94707ff20e90
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-04-04 16:08:46 +0000

    add backend

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '23d71b307b199d63e320cc3542c812e2aea2a98c',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '23d71b307b199d63e320cc3542c812e2aea2a98c',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.2.1',
+            'version' => '5.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'f42c9110abe98dd6cfe9053c49bc86acc70b2d23',
+            'dev_requirement' => false,
+        ),
+    ),
+);
